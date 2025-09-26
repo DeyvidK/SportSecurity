c11531d498979f0f7938d918f727edb2a87d126e
Deyvid Rangel <deyvidkessio@gmai.com>
2025-09-26 11:49:00 -0300

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
 import { CorpoComponent } from './components/main/corpo/corpo.component';
+import { DescricaoComponent } from './components/descricao/descricao.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,CabecalhoComponent,CorpoComponent],
+  imports: [RouterOutlet,CabecalhoComponent,CorpoComponent,DescricaoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
