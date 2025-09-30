0d4627344fa391651d718daa0d11bc848a2ac899
Deyvid Rangel <deyvidkessio@gmail.com>
2025-09-30 18:48:10 -0300

    inclusão plano

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
 import { CorpoComponent } from './components/main/corpo/corpo.component';
 import { DescricaoComponent } from './components/descricao/descricao.component';
+import { PlanosComponent } from './components/planos/planos.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,CabecalhoComponent,CorpoComponent,DescricaoComponent],
+  imports: [RouterOutlet,CabecalhoComponent,CorpoComponent,DescricaoComponent,PlanosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
