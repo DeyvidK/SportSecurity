6e85c77682e122b6585968f2b3a9cfc57ce89137
Deyvid Rangel <deyvidkessio@gmail.com>
2025-10-02 21:57:03 -0300

    update planos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
 import { CorpoComponent } from './components/main/corpo/corpo.component';
 import { DescricaoComponent } from './components/descricao/descricao.component';
 import { PlanosComponent } from './components/planos/planos.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,CabecalhoComponent,CorpoComponent,DescricaoComponent,PlanosComponent],
+  imports: [RouterOutlet,CabecalhoComponent,CorpoComponent,DescricaoComponent,PlanosComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
